aedb078a01ab7772c571ce0933a530c86a378620
Eli Uc <edictxsubmission@gmail.com>
2018-07-20 16:37:48 -0700

    added an add function to LL class

--- p1.py
+++ p1.py
@@ -11,3 +11,10 @@ class ListNode(object):
         self.val = x
         self.next = None
         
+class Fake_LL(object):
+    def __init__(self,x):
+        self.head = None
+    def add(self,x):
+        new = ListNode(x)
+        new.next = self.head
+        self.head = new
